afe82671bcc03998531189b3dbb3465decc59619
Mark Semotiuk <marksem@gmail.com>
2018-07-25 19:04:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++){
-  if (i === 1) {array.unshift(`I am ${i} strange loop.`)} else {array.unshift(`I am ${i} strange loops.`)}
+  if (i == 1) {array.unshift(`I am ${i} strange loop.`)} else {array.unshift(`I am ${i} strange loops.`)}
   }
 }
 
