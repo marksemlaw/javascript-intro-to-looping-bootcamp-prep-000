e4495d3ec59621802b2fe1faf7c37055cb467375
Mark Semotiuk <marksem@gmail.com>
2018-07-25 18:43:04 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array) {
+  for(let i = 0; i < 25; i++){
+  if (i === 1) {array.unshift(`I am ${i} strange loop.`)} else {array.unshift(`I am ${i} strange loops.`)}
+  }
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+  console.log(number)
+  number--
+  }
+  }
+  
+  
