80d6cdcbd1b467b6cf43103e6b53114108713b28
Mark Semotiuk <marksem@gmail.com>
2018-07-25 19:07:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++){
   if (i == 1) {array.push(`I am ${i} strange loop.`)} else {array.push(`I am ${i} strange loops.`)}
-  return array}
-}
+  }
+return array}
 
 function whileLoop(number) {
 while (number > 0) {console.log(number--)}
